8478ff6f33d77fe4d024c9ba2c07edf09c0623e8
bennkingy <bennkingy@gmail.com>
2024-03-11 16:33:06 +0000

    added copy all ids button

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,48 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 function App() {
   const [ids, setIds] = useState([]);
+  const dialogRef = useRef(null);
 
   const identifiers = ['WD', 'CATH', 'SKIP', 'SEARCH', 'ENT'];
   const regexPattern = `\\b(${identifiers.join('|')})\\s?-\\s?\\d{3,6}\\b`;
   const regex = new RegExp(regexPattern, 'g');
 
   const onChange = (text) => {
-    if(!text) {
+    if (!text) {
       setIds([]);
+      return;
     }
+
     let matchedIds = text.match(regex);
+
+    if (!matchedIds) {
+      setIds([]);
+      return;
+    }
+
     matchedIds = matchedIds.map(id => id.replace(/\s?-\s?/, '-'));
-    const uniqueIds = matchedIds ? Array.from(new Set(matchedIds)).sort() : [];
-    setIds(uniqueIds || []);
+    const uniqueIds = Array.from(new Set(matchedIds)).sort();
+
+    setIds(uniqueIds);
+  };
+
+  const copyToClipboard = () => {
+    const idsToCopy = ids.join('\n');
+    navigator.clipboard.writeText(idsToCopy).then(() => {
+      if (dialogRef.current) {
+        
+        dialogRef.current.showModal();
+
+        setTimeout(() => {
+          dialogRef.current.close();
+        }, 1500);
+
+      }
+    }).catch(err => {
+      console.error('Failed to copy: ', err);
+    });
   };
 
   return (
@@ -28,16 +55,27 @@ function App() {
         onChange={(e) => onChange(e.target.value)}
       />
       {ids.length > 0 ? (
-      <>
-      <h2 style={{marginBottom: -10, marginTop: 40}}>IDS</h2>
-      <ul style={{paddingInlineStart: 0}}>
-        {ids.map((id, index) => (
-          <li key={index} style={{ listStyle: 'none' }}>
-            {id}
-          </li>
-        ))}
-      </ul>
-      </>) : null}
+        <>
+          <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginBottom: 10, marginTop: 40}}>
+            <h2 style={{margin: 0, marginRight: 20}}>IDS</h2>
+            <button
+              onClick={copyToClipboard}
+              title="Click to copy all IDs to clipboard"
+              style={{ cursor: 'pointer'}}
+            >
+              Copy All IDs
+            </button>
+          </div>
+          <ul style={{paddingInlineStart: 0}}>
+            {ids.map((id, index) => (
+              <li key={index} style={{ listStyle: 'none' }}>
+                {id}
+              </li>
+            ))}
+          </ul>
+        </>
+      ) : null}
+      <dialog ref={dialogRef}>IDs copied to clipboard!</dialog>
     </>
   );
 }
